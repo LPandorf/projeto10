74712b1f0e22b6ae332e004e2ec9250a2ec15608
LPandorf <leticiapandorf@gmail.com>
2022-10-06 20:33:46 -0300

    function filmes

--- src/components/Filme.js
+++ src/components/Filme.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export default function Filme({filme}){
     return (
         <>
-            <Link to={}>
-                
+            <Link to={'/sessoes/${filme.id'}>
+                <div className="filme">
+                    <img src={filme.cartazUrl}/>
+                </div>                
             </Link>
         </>
     );
--- src/components/Filmes.js
+++ src/components/Filmes.js
@@ -1,12 +1,38 @@
+import styled from 'styled-components';
 import { useState, useEffect } from "react";
 import filme from "./Filme";
+import { pegarFilmes } from "./Apis";
 
 //função para pegar a lista de filmes e mostrar na tela
 
 export default function Filmes(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {pegarFilmes().then((res) => setFilmes(res.data));}, []);
+        
+
     return(
         <>
-        
+            <Titulo>Selecione o Filme</Titulo>
+            <Catalogo>
+                {filmes.map((m) => (
+                    <Filme key={m.id} movie={m}/>
+                ))}
+            </Catalogo>
         </>
     );
 }
+
+const Titulo = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 110px;
+    font-size: 24px;
+`
+const Catalogo = styled.div`
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    cursor: pointer;
+`
